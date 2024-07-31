d422fb258d4eb6c77435a2c781db3e9ce0b57817
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-07-31 10:47:49 +0700

    Feat(chore): Set up package, router

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv/config");
 
 const app = express();
 const env = process.env;
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+app.use(cors());
+app.options("*", cors());
+// router
+const authRouter = require("./routes/auth.route.js");
 
 const host = env.HOST;
 const port = env.PORT;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDb");
+  })
+  .catch((e) => console.log(e));
 // start server
 app.listen(port, host, () => {
   console.log(`App is running at https://${host}:${port}`);
